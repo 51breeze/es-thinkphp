b96ad5a64bcde0fa1a81f6a1eb3d9a1ccb478123
664371281@qq.com <664371281@qq.com>
2022-11-04 10:48:46 +0800

    fix

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ class Plugin extends PluginPHP{
         this.name = pkg.name;
         this.version = pkg.version;
         this.platform = 'server';
-        const dir = path.join(__dirname,'types');
-        const files = fs.readdirSync( dir ).filter( item=>!(item === '.' || item === '..') ).map( item=>path.join(dir,item) );
-        compiler.loadTypes(files,true,null,true);
-        this.globalTypes = (compiler.options.globalTypes || []).map( file=>compiler.normalizePath(file) );
-        registerError(compiler.diagnostic.defineError, compiler.diagnostic.LANG_CN, compiler.diagnostic.LANG_EN );
+        // const dir = path.join(__dirname,'types');
+        // const files = fs.readdirSync( dir ).filter( item=>!(item === '.' || item === '..') ).map( item=>path.join(dir,item) );
+        // compiler.loadTypes(files,true,null,true);
+        // this.globalTypes = (compiler.options.globalTypes || []).map( file=>compiler.normalizePath(file) );
+        // registerError(compiler.diagnostic.defineError, compiler.diagnostic.LANG_CN, compiler.diagnostic.LANG_EN );
     }
 
     getTokenNode(name, flag){
